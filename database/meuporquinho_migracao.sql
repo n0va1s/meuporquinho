40c381bc99af09907d566e638fe3346756b51e61
João Paulo Novais <joaopaulonovais@gmail.com>
2016-08-30 18:32:36 -0300

    Ajustes na rotina de migracao

--- database/meuporquinho_migracao.sql
+++ database/meuporquinho_migracao.sql
@@ -2,32 +2,23 @@ INSERT INTO usuario(nom_usuario,eml_usuario,val_senha,tip_genero,dat_nascimento)
 INSERT INTO usuario(nom_usuario,eml_usuario,val_senha,tip_genero,dat_nascimento) VALUES("João Paulo Cirino Silva de Novais","jp.pessoal@gmail.com",md5("linux1"), "M", "1980-07-01");
 
 INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('MIG','Migração'); 
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('FIX','Gastos fixos');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('HAB','Habitação');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('SAU','Saúde');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('AUT','Automóvel');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('PES','Pessoal');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('LAZ','Lazer');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('DEP','Dependentes');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('INV','Investimento');
-INSERT INTO categoria_grupo(tip_grupo, nom_grupo) VALUES('TAX','Impostos e taxas');
 
 alter table categoria add column tipo_id smallint comment 'Identificador da categoria na versao anterior';
 
 insert into categoria (nom_categoria,txt_categoria,ind_categoria,tip_grupo,seq_usuario,tipo_id)
-select descricao, 'Sugestão de gastos para esta categoria','R', 'MIG',2,id
+select descricao, 'Sugestão de gastos para esta categoria','R', 'MIG',(select seq_usuario from usuario where eml_usuario = 'jp.pessoal@gmail.com'),id
 from tipo
 where tipo = 1;
 
 insert into categoria (nom_categoria,txt_categoria,ind_categoria,tip_grupo,seq_usuario,tipo_id)
-select descricao, 'Sugestão de gastos para esta categoria','D', 'MIG',2,id
+select descricao, 'Sugestão de gastos para esta categoria','D', 'MIG',(select seq_usuario from usuario where eml_usuario = 'jp.pessoal@gmail.com'),id
 from tipo
 where tipo = 0;
 
 alter table regra add column tipo_id smallint comment 'Identificador da categoria na versao anterior';
 
 insert into regra(seq_usuario,des_regra,seq_categoria,tipo_id)
-select 2,descricao,1,tipo
+select (select seq_usuario from usuario where eml_usuario = 'jp.pessoal@gmail.com'),descricao,(select seq_categoria from categoria limit 1),tipo
 from tipo_palavras;
 
 alter table lancamento add column tipo_id smallint comment 'Identificador da categoria na versao anterior';
@@ -35,7 +26,9 @@ alter table lancamento add column tipo_id smallint comment 'Identificador da cat
 insert into lancamento(seq_usuario,seq_categoria,dat_lancamento,mes_lancamento,
                        ano_lancamento,val_lancamento,txt_lancamento,tip_origem,
                        nom_origem,txt_origem,cod_identificacao,tipo_id)
-select 2,1,concat_ws('-',substr(data,7,4),substr(data,4,2),substr(data,1,2)),mes,
+select (select seq_usuario from usuario where eml_usuario = 'jp.pessoal@gmail.com'),
+        (select seq_categoria from categoria limit 1),
+        concat_ws('-',substr(data,7,4),substr(data,4,2),substr(data,1,2)),mes,
        ano,valor,item,case tiporegistro 
                          when 'Cartão de Crédito' then 'CRE'
                          when 'Débito em Conta' then 'DEB'
@@ -47,8 +40,11 @@ from tarefas;
 update lancamento set val_lancamento = val_lancamento * -1 
 where val_lancamento < 0;
 
+update regra set seq_categoria = 
+(select seq_categoria from categoria where categoria.tipo_id = regra.tipo_id);
+
 update lancamento set seq_categoria = 
-(select seq_categoria from categoria where categoria.tipo_id = lancamento.tipo_id)
+(select seq_categoria from categoria where categoria.tipo_id = lancamento.tipo_id);
 
 /*
 Conferindo
